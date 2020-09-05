994c5a308afc0e7bd409205b860fbb91eedd1507
Muyinda ROgers <moverr@gmail.com>
2020-09-06 01:24:00 +0300

    Changing from Discovery client to Feign

--- src/main/java/com/lab/subject/controllers/Controller.java
+++ src/main/java/com/lab/subject/controllers/Controller.java
@@ -5,6 +5,10 @@
  */
 package com.lab.subject.controllers;
 
+import com.lab.subject.feign.Lab4Adjective;
+import com.lab.subject.feign.Lab4Article;
+import com.lab.subject.feign.Lab4Noun;
+import com.lab.subject.feign.Lab4Verb;
 import com.lab.subject.feign.Lab4subject;
 import java.net.URI;
 import java.util.List;
@@ -30,6 +34,18 @@ public class Controller {
     @Autowired
     Lab4subject lab_subject;
 
+    @Autowired
+    Lab4Adjective lab_adjective;
+
+    @Autowired
+    Lab4Article lab_article;
+
+    @Autowired
+    Lab4Noun lab_noun;
+
+    @Autowired
+    Lab4Verb lab_verb;
+
     @GetMapping("/sentence")
     public @ResponseBody
     String getSentence() {
@@ -48,18 +64,26 @@ public class Controller {
         switch (service) {
             case "LAB-4-SUBJECT":
                 return lab_subject.getVerb();
+            case "LAB-4-VERB":
+                return lab_verb.getVerb();
+            case "LAB-4-ARTICLE":
+                return lab_article.getVerb();
+            case "LAB-4-ADJECTIVE":
+                return lab_adjective.getVerb();
+            case "LAB-4-NOUN":
+                return lab_noun.getVerb();
             default:
-                List<ServiceInstance> list = client.getInstances(service);
-                if (list != null && list.size() > 0) {
-                    URI uri = list.get(0).getUri();
-                    if (uri != null) {
-                        String st = (new RestTemplate()).getForObject(uri, String.class);
-                        return st;
-                    }
-                }
+                /* List<ServiceInstance> list = client.getInstances(service);
+                 if (list != null && list.size() > 0) {
+                 URI uri = list.get(0).getUri();
+                 if (uri != null) {
+                 String st = (new RestTemplate()).getForObject(uri, String.class);
+                 return st;
+                 }
+                 }*/
+                return null;
         }
 
-        return null;
     }
 
 }
---
+++ src/main/java/com/lab/subject/feign/Lab4Article.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lab.subject.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author muyins
+ */
+ 
+@FeignClient(value = "LAB-4-ARTICLE")
+public interface Lab4Article {
+    
+    @RequestMapping(method=RequestMethod.GET,value = "/")
+    String getVerb();
+}
